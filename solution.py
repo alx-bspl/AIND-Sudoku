4f0ae2d7d86ae8f053f6befe2b053cde0ecf848a
alexb5dh <alexb.5dh@gmail.com>
2017-04-20 20:52:39 +0300

    cleanup
    - delete redundant 'assign_value'

--- solution.py
+++ solution.py
@@ -9,23 +9,13 @@ class VisualizibleDict(dict):
         Assigns a value to a given box. If it updates the board record it.
         """
 
+        # Don't waste memory appending actions that don't actually change any values
         if self[box] == value:
             return
 
         super().__setitem__(box, str(value))
         if len(value) == 1:
             self.assignments.append(self.copy())
-
-def assign_value(values, box, value):
-
-    # Don't waste memory appending actions that don't actually change any values
-    if values[box] == value:
-        return values
-
-    values[box] = value
-    if len(value) == 1:
-        assignments.append(values.copy())
-    return values
 
 def cross(A, B):
     "Cross product of elements in A and elements in B."
